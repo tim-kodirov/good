6fa8c0d06b1a98a1a3500cc195193228e4ad1e05
Erkin <erkinboy.botirov@mail.ru>
2017-09-07 16:31:24 +0500

    active menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,8 +35,8 @@
                 <div class="col-sm-2">
                     <ul class="nav nav-pills nav-stacked" data-spy="affix" data-offset-top="20">
                       <li class="{{Request::is('store') ? 'active' : ''}}"><a href="{{ route('store') }}">Остаток</a></li>
-                      <li class="{{Request::is('store.export') ? 'active' : ''}}"><a href="{{ route('store.export') }}">Расходы</a></li>
-                      <li class="{{Request::is('store.import') ? 'active' : ''}}"><a href="{{ route('store.import') }}">Приходы</a></li>
+                      <li class="{{Request::is('store/export') ? 'active' : ''}}"><a href="{{ route('store.export') }}">Расходы</a></li>
+                      <li class="{{Request::is('store/import') ? 'active' : ''}}"><a href="{{ route('store.import') }}">Приходы</a></li>
                     </ul>
                 </div>
 
