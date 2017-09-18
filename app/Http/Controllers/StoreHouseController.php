da4086c0df1da58b4a137c5c8108d3612eeec3cb
Erkin <erkinboy.botirov@mail.ru>
2017-09-19 01:14:03 +0500

    store.remainder is ready except client.contacts

--- app/Http/Controllers/StoreHouseController.php
+++ app/Http/Controllers/StoreHouseController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
 use App\Export;
+use App\Import;
 use App\Product;
+use App\Remainder;
 use App\Storehouse;
+use App\Request as OfficeRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,6 +22,7 @@ class StoreHouseController extends Controller
     public function getIndex()
     {
         $storehouse = Auth::user();
+        $clients = Client::all();
         $goods = array();
         foreach($storehouse->products as $product)
         {
@@ -29,7 +34,7 @@ class StoreHouseController extends Controller
             $good['requests']['down'] = array();
             foreach($product->requests as $request)
             {
-                if($request->isExport && $request->isActive)
+                if($request->isExport && $request->isActive == 1)
                 {
                     $up = array();
                     $up['id'] = $request->id;
@@ -38,7 +43,7 @@ class StoreHouseController extends Controller
                     $up['date'] = $request->created_at;
                     array_push($good['requests']['up'],$up);
                 }
-                elseif(!$request->isExport && $request->isActive)
+                elseif(!$request->isExport && $request->isActive == 1)
                 {
                     $down = array();
                     $down['id'] = $request->id;
@@ -55,7 +60,8 @@ class StoreHouseController extends Controller
             array_push($goods,$good);
         }
         $goods = json_encode($goods);
-    	return view('storehouse.remainder')->withGoods($goods);
+        $clients->toJson();
+    	return view('storehouse.remainder')->withGoods($goods)->withClients($clients);
     }
 
     public function getImport()
@@ -70,40 +76,113 @@ class StoreHouseController extends Controller
 
     public function productExport(Request $request)
     {
-        $storehouse = Auth::user();
-        $product = $storehouse->products()->findOrFail($request->export_product_id);
-        $product->remainder->quantity = $product->remainder->quantity - $request->export_product_quantity;
-        $product->remainder->save();
-        $export = new Export;
-        $export->remainder_id = $product->remainder->id;
-        $export->quantity = $request->export_product_quantity;
-        $export->fromRequest = false;
-        $export->save();
+        if(!empty($request->export_product_quantity) && !empty($request->export_client_name))
+        {
+            $storehouse = Auth::user();
+            $product = $storehouse->products()->findOrFail($request->export_product_id);
+            $product->remainder->quantity = $product->remainder->quantity - $request->export_product_quantity;
+            $product->remainder->save();
+            $export = new Export;
+            $export->remainder_id = $product->remainder->id;
+            if(Client::where('name',$request->import_client_name)->exists()){
+                $client = Client::where('name',$request->import_client_name)->get();
+                $export->client_id = $client->id;
+            }
+            else{
+                $client = new Client;
+                $client->name = $request->import_client_name;
+                $client->contacts = "?";//till implementation
+                $client->save();
+                $export->client_id = $client->id;
+            }
+            $export->quantity = $request->export_product_quantity;
+            $export->fromRequest = false;
+            $export->save();
+        }
+        return back();
     }
 
     public function productImport(Request $request)
     {
-
+        if(!empty($request->import_product_quantity) && !empty($request->import_client_name))
+        {
+            $storehouse = Auth::user();
+            $product = $storehouse->products()->findOrFail($request->import_product_id);
+            $product->remainder->quantity = $product->remainder->quantity + $request->import_product_quantity;
+            $product->remainder->save();
+            $import = new Import;
+            $import->remainder_id = $product->remainder->id;
+            if(Client::where('name',$request->import_client_name)->exists()){
+                $client = Client::where('name',$request->import_client_name)->get();
+                $import->client_id = $client->id;
+            }
+            else{
+                $client = new Client;
+                $client->name = $request->import_client_name;
+                $client->contacts = "?";//till implementation
+                $client->save();
+                $import->client_id = $client->id;
+            }
+            $import->quantity = $request->import_product_quantity;
+            $import->fromRequest = false;
+            $import->save();
+        }
+        return back();
     }
     /*Export made according to request from office*/
     public function requestExportAccept(Request $request)
     {
-
+        if(!empty($request->selected_requests_id))
+        {
+            $exportRequests = OfficeRequest::whereIn('id',$request->selected_requests_id)->get();
+            foreach($exportRequests as $exportRequest)
+            {
+                $remainder = Remainder::findOrFail($exportRequest->remainder_id);
+                $remainder->quantity = $remainder->quantity - $exportRequest->quantity;
+                $remainder->save();
+                $export = new Export;
+                $export->remainder_id = $remainder->id;
+                $export->client_id = $exportRequest->client_id;
+                $export->quantity = $exportRequest->quantity;
+                $export->fromRequest = true;
+                $export->save();
+                $exportRequest->isActive = false;
+                $exportRequest->save();
+            }
+        }
+        return back();
     }
     /*Import according to request from office*/
     public function requestImportAccept(Request $request)
     {
-
+        if(!empty($request->selected_requests_id)) {
+            $importRequests = OfficeRequest::whereIn('id', $request->selected_requests_id)->get();
+            foreach ($importRequests as $importRequest) {
+                $remainder = Remainder::findOrFail($importRequest->remainder_id);
+                $remainder->quantity = $remainder->quantity + $importRequest->quantity;
+                $remainder->save();
+                $import = new Import;
+                $import->remainder_id = $remainder->id;
+                $import->client_id = $importRequest->client_id;
+                $import->quantity = $importRequest->quantity;
+                $import->fromRequest = true;
+                $import->save();
+                $importRequest->isActive = false;
+                $importRequest->save();
+            }
+        }
+        return back();
     }
 
-    public function requestExportReject(Request $request)
+    public function requestReject(Request $request)
     {
-
-    }
-
-    public function requestImportReject(Request $reject)
-    {
-
+        $officeRequests = OfficeRequest::where('id',$request->selected_requests_id)->get();
+        foreach($officeRequests as $officeRequest)
+        {
+            $officeRequest->isActive = 2;//which means Storehouse rejected Office request
+            $officeRequest->save();
+        }
+        return back();
     }
     /*Add new product to list*/
     public function createProduct(Request $request)
--- resources/views/storehouse/remainder.blade.php
+++ resources/views/storehouse/remainder.blade.php
@@ -25,11 +25,12 @@
 
         app.controller('myCtrl', function($scope) {
             $scope.goods = {!! $goods !!},
-            $scope.whos = [
-                {name: 'Temur Kodirov', contact: '191-01-02' },
-                {name: 'Alisher Kodirov', contact: '191-01-02' },
-                {name: 'Ulug Kodirov', contact: '191-01-02' },
-            ]
+            $scope.whos = {!! $clients !!}
+//            $scope.whos = [
+//                {name: 'Temur Kodirov', contacts: '191-01-02' },
+//                {name: 'Alisher Kodirov', contacts: '191-01-02' },
+//                {name: 'Ulug Kodirov', contacts: '191-01-02' },
+//            ]
 
 //            $scope.goods = [
 //                { name: 'Audi 02/3421/23', number: 234, requests: {up: false, down: false} },
@@ -128,7 +129,7 @@
                                                 <button class = "btn btn-info btn-block" data-dismiss="modal" aria-hidden="true">Отмена</button>
                                             </div>
                                             <div class="col-md-4">
-                                                <button type="submit" formaction="{{route('store.request.export.reject')}}" class = "btn btn-danger btn-block">Отклонить</button>
+                                                <button type="submit" formaction="{{route('store.request.reject')}}" class = "btn btn-danger btn-block">Отклонить</button>
                                             </div>
                                         </div>
                                     </div>
@@ -182,7 +183,7 @@
                                                 <button class = "btn btn-info btn-block" data-dismiss="modal" aria-hidden="true">Отмена</button>
                                             </div>
                                             <div class="col-md-4">
-                                                <button formaction="{{route('store.request.import.reject')}}" class = "btn btn-danger btn-block">Отклонить</button>
+                                                <button formaction="{{route('store.request.reject')}}" class = "btn btn-danger btn-block">Отклонить</button>
                                             </div>
                                         </div>
                                     </div>
@@ -200,7 +201,8 @@
                 <div class="modal fade" id="download@{{$index}}">
                     <div class="modal-dialog">
                         <div class="modal-content">
-                            <form method="post" action="">
+                            <form method="post" action="{{route('store.product.import')}}">
+                                {{csrf_field()}}
                                 <div class="modal-header">
                                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                     <h4 class="modal-title text-center">Приход</h4>
@@ -224,7 +226,7 @@
                                             <tr>
                                                 <th>От кого</th>
                                                 <td>
-                                                    <input type = "text" list = "who_down" class = "form-control" />
+                                                    <input type = "text" list = "who_down" class = "form-control" name="import_client_name"/>
                                                     <datalist id = "who_down">
                                                         <option ng-repeat = "who in whos | orderBy: 'name' ">@{{who.name}}</option>
                                                     </datalist>
@@ -277,7 +279,7 @@
                                             <tr>
                                                 <th>Кому</th>
                                                 <td>
-                                                    <input type = "text" list = "who_up" class = "form-control" />
+                                                    <input type = "text" list = "who_up" class = "form-control" name="export_client_name"/>
                                                     <datalist id = "who_up">
                                                         <option ng-repeat = "who in whos | orderBy: 'name' ">@{{who.name}}</option>
                                                     </datalist>
